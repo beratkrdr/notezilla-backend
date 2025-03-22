a675007f19e75979474eb08de7522c63aff5b93f
Berat Kırdar <beratkrdr@hotmail.com>
2025-03-22 18:19:26 +0300

    Initial commit

---
+++ src/main/java/com/beratkirdar/notezilla/NotezillaApplication.java
@@ -0,0 +1,13 @@
+package com.beratkirdar.notezilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotezillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotezillaApplication.class, args);
+	}
+
+}
